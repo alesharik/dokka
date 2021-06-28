6a32a76044ee5885b24962007b2c894d065ca7f6
Marcin Aman <marcin.aman@gmail.com>
2021-06-28 09:32:48 +0200

    Change jekyll links to be .html

---
+++ plugins/jekyll/jekyll-template-processing/src/main/kotlin/org/jetbrains/dokka/gfm/templateProcessing/JekyllTemplateProcessingPlugin.kt
@@ -0,0 +1,36 @@
+package org.jetbrains.dokka.gfm.templateProcessing
+
+import org.jetbrains.dokka.allModulesPage.AllModulesPagePlugin
+import org.jetbrains.dokka.allModulesPage.MultimoduleLocationProvider
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.resolvers.local.DokkaLocationProviderFactory
+import org.jetbrains.dokka.gfm.GfmPlugin
+import org.jetbrains.dokka.jekyll.JekyllPlugin
+import org.jetbrains.dokka.plugability.DokkaPlugin
+
+class JekyllTemplateProcessingPlugin : DokkaPlugin() {
+
+    private val allModulesPagePlugin by lazy { plugin<AllModulesPagePlugin>() }
+
+    private val jekyllPlugin by lazy { plugin<JekyllPlugin>() }
+    private val gfmPlugin by lazy { plugin<GfmPlugin>() }
+    private val gfmTemplatingPlugin by lazy { plugin<GfmTemplateProcessingPlugin>() }
+
+    private val dokkaBase by lazy { plugin<DokkaBase>() }
+
+    val jekyllLocationProvider by extending {
+        dokkaBase.locationProviderFactory providing MultimoduleLocationProvider::Factory override listOf(
+            jekyllPlugin.locationProvider,
+            gfmPlugin.locationProvider,
+            gfmTemplatingPlugin.gfmLocationProvider,
+            allModulesPagePlugin.multimoduleLocationProvider
+        )
+    }
+
+    val jekyllPartialLocationProvider by extending {
+        allModulesPagePlugin.partialLocationProviderFactory providing ::DokkaLocationProviderFactory override listOf(
+            allModulesPagePlugin.baseLocationProviderFactory,
+            gfmTemplatingPlugin.gfmPartialLocationProvider
+        )
+    }
+}
